171391cdb9d71f45b62732046f3efe3089bd7a70
jbcazaux <jbcazaux@gmail.com>
2018-09-14 00:08:36 +0200

    Gifs as a functional component

--- src/Gifs.js
+++ src/Gifs.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Gif from "./Gif";
 
-export default class Gifs extends  React.Component {
-  render() {
-    return <div className="gifs">
-      {this.props.gifs.map(gif => <Gif gif={gif} key={gif.id}/>)}
+export default ({gifs}) => (
+    <div className="gifs">
+      {gifs.map(gif => <Gif gif={gif} key={gif.id}/>)}
     </div>
-  }
-}
+)
